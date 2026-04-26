e3fa0ed124db4a880f86e2188d7f27d579372be6
Rajapandian <rajapandian21@gmail.com>
2026-04-26 17:40:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
 
     
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 sf_df = st.datafram(data=smoothiefroot_response.json(), use_container_width=True)
